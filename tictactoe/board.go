9a3ab7849f947d09c698edebfd622d15bd127edc
Dom Leone <ohheydom@gmail.com>
2014-07-29 00:30:21 -0400

    Initial Commit

---
+++ tictactoe/board.go
@@ -0,0 +1,5 @@
+package tictactoe
+
+func CreateBoard() []string {
+  return []string { "-", "-", "-", "-", "-", "-", "-", "-", "-" }
+}
---
+++ tictactoego.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "./tictactoe/"
+)
+
+func main() {
+	fmt.Println("test")
+  fmt.Println(tictactoe.CreateBoard())
+}
