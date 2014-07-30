d9ef49b242e09db204000034c86c9ab913c3ba7b
Dom Leone <ohheydom@gmail.com>
2014-07-29 23:14:50 -0400

    Minor tweak to the display class.

--- tictactoe/display.go
+++ tictactoe/display.go
@@ -6,12 +6,17 @@ import (
   "strings"
 )
 
+func horizontal_bars(dimension int) {
+  fmt.Println("---" + strings.Repeat("-", (dimension * 2)) + "--")
+}
+
 func DisplayBoard(board []string) {
   dimension := int(math.Sqrt(float64(len(board))))
-  fmt.Println("---" + strings.Repeat("-", (dimension * 2)) + "--")
-  for i := 0; i < dimension; i++ {
-    top := i + dimension
-    fmt.Println("| ", strings.Join(board[i:top], " "), " |")
+  horizontal_bars(dimension)
+  for i := 0; i < len(board); {
+    row_end := i + dimension
+    fmt.Println("| ", strings.Join(board[i:row_end], " "), " |")
+    i += dimension
   }
-  fmt.Println("---" + strings.Repeat("-", (dimension * 2)) + "--")
+  horizontal_bars(dimension)
 }
