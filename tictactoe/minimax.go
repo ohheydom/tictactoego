ae49b3b31c0e0977f6daeabceb5816d693b191f7
Dom Leone <ohheydom@gmail.com>
2014-07-31 17:20:23 -0400

    Working on Minimax function.

--- tictactoe/minimax.go
+++ tictactoe/minimax.go
@@ -1,5 +1,48 @@
 package tictactoe
 
-func MiniMax() {
+func max(x, y int) int {
+  if x > y {
+    return x
+  }
+  return y
+}
 
+func min(x, y int) int {
+  if x < y {
+    return x
+  }
+  return y
+}
+
+func MiniMax(g GameBoard, depth int, max_player bool) int {
+  if g.Win() {
+    if g.PreviousTurn() == "X" {
+      return 100 - depth
+    }
+    return depth - 100
+  }
+  if len(g.RemainingIndices()) == 0 {
+    return 0
+  }
+
+  best_value := -1000
+
+  if max_player {
+    best_value = -1000
+    for _, move := range g.RemainingIndices() {
+      g.Move(move, "X")
+      value := MiniMax(g, depth - 1, false)
+      best_value = max(best_value, value)
+      return best_value
+    }  
+  } else {
+    best_value = 1000
+    for _, move := range g.RemainingIndices() {
+      g.Move(move, "O")
+      value := MiniMax(g, depth -1, true)
+      best_value = min(best_value, value)
+      return best_value
+    }
+  }
+  return 0
 }
--- tictactoe/minimax_test.go
+++ tictactoe/minimax_test.go
@@ -3,5 +3,21 @@ package tictactoe
 import "testing"
 
 func TestMiniMax(t *testing.T) {
-  t.Error("Nothing written yet")
+  win_x := []string{"X", "X", "X", "-", "O", "O", "O", "-", "-"}
+  win_o := []string{"O", "X", "X", "-", "O", "-", "-", "-", "O"}
+  g_x := GameBoard{Board: win_x, Turn: "O"}
+  g_o := GameBoard{Board: win_o, Turn: "X"}
+  expected_minimax_win_x := 100
+  minimax_win_x := MiniMax(g_x, 0, true)
+  expected_minimax_win_o := -100
+  minimax_win_o := MiniMax(g_o, 0, true)
+
+  if expected_minimax_win_x != minimax_win_x {
+    t.Error("Expected: ", expected_minimax_win_x, "Got: ", minimax_win_x)
+  }
+
+  if expected_minimax_win_o != minimax_win_o {
+    t.Error("Expected: ", expected_minimax_win_o, "Got: ", minimax_win_o)
+  }
+
 }
