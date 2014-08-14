aba889f990070215adc5c5b2dd1292966073d1ae
Dom Leone <ohheydom@gmail.com>
2014-08-14 00:01:55 -0400

    Sped up ValidMove and CreateBoard slightly.

--- tictactoe/board.go
+++ tictactoe/board.go
@@ -7,7 +7,7 @@ type GameBoard struct {
 
 func CreateBoard(size int) []string {
   dim := size * size
-  var board []string
+  board := make([]string, 0, dim)
   for i := 0; i < dim; i++ {
     board = append(board, "-")
   }
@@ -52,9 +52,15 @@ func (g *GameBoard) UndoMove(ind int) {
 }
 
 func ValidMove(remaining_indices []int, move int) bool {
-  for _, valid_move := range remaining_indices {
-    if valid_move == move {
-      return true
+  n := len(remaining_indices) / 2
+  if remaining_indices[n] == move { return true }
+  if remaining_indices[n] > move {
+    for _, valid_move := range remaining_indices[:n] {
+      if valid_move == move { return true }
+    }
+  } else {
+    for _, valid_move := range remaining_indices[n:] {
+      if valid_move == move { return true }
     }
   }
   return false
--- tictactoe/board_test.go
+++ tictactoe/board_test.go
@@ -22,3 +22,10 @@ func BenchmarkRemainingIndices(b *testing.B) {
     g.RemainingIndices()
   }
 }
+
+func BenchmarkValidMove(b *testing.B) {
+  remaining_indices := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+  for i := 0; i < b.N; i++ {
+    ValidMove(remaining_indices, 7)
+  }
+}
